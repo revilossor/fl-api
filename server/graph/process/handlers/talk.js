3714598a21af4b486bbc1333b7dba2e01b8b6696
Oliver Ross <oliver.ross01@bbc.co.uk>
2018-12-23 22:20:44 +0000

    talk node

--- server/graph/process/handlers/talk.js
+++ server/graph/process/handlers/talk.js
@@ -4,5 +4,17 @@ const getSubGraph = require('../getSubGraph')
 module.exports = (graph, state) => {
   const scope = getSubGraph(graph, state.path)
   const children = getChildren(scope, state)
-  return { ...state, current: children[0] }
+  const node = scope.processes[state.current]
+
+  const text = state.text ? [ ...state.text ] : []
+  if (node.metadata.text) {
+    text.push(node.metadata.text)
+  }
+
+  const audio = state.audio ? [ ...state.audio ] : []
+  if (node.metadata.audio) {
+    audio.push(node.metadata.audio)
+  }
+
+  return { ...state, current: children[0], text, audio }
 }
---
+++ server/graph/process/handlers/talk.test.js
@@ -0,0 +1,110 @@
+const handler = require('./talk')
+
+const graph = require('../../../../fixtures/node_types')
+const state = { current: 'talk_one', path: [] }
+
+let updated
+
+describe('traversal', () => {
+  beforeAll(() => {
+    updated = handler(graph, state)
+  })
+
+  it('the updated current is the next node', () => {
+    expect(updated.current).toBe('question')
+  })
+
+  it('the updated path is the input path', () => {
+    expect(updated.path).toEqual([])
+  })
+
+})
+
+describe('text', () => {
+
+  const stateWithText = { ...state, text: ['sometext'] }
+
+  describe('if there is text in the node', () => {
+
+    it('if there is text in state, node text is appended to it', () => {
+      updated = handler(graph, stateWithText)
+      expect(updated.text).toEqual([
+        ...stateWithText.text,
+        'talk_one_text'
+      ])
+    })
+
+    it('if there is no text in state, its created containing node text', () => {
+      updated = handler(graph, state)
+      expect(updated.text).toEqual([
+        'talk_one_text'
+      ])
+    })
+  })
+
+  describe('if there is no text in the node', () => {
+
+    const noTextGraph = JSON.parse(JSON.stringify(graph))
+
+    beforeAll(() => {
+      delete noTextGraph.processes.talk_one.metadata.text
+    })
+
+    it('if there is text in state, it remains the same', () => {
+      updated = handler(noTextGraph, stateWithText)
+      expect(updated.text).toEqual(stateWithText.text)
+    })
+
+    it('if there is no text in state, its created empty', () => {
+      updated = handler(noTextGraph, state)
+      expect(updated.text).toEqual([])
+    })
+
+  })
+
+})
+
+describe('audio', () => {
+
+  const stateWithAudio = { ...state, audio: ['someaudio'] }
+
+  describe('if there is audio in the node', () => {
+
+    it('if there is audio in state, node audio is appended to it', () => {
+      updated = handler(graph, stateWithAudio)
+      expect(updated.audio).toEqual([
+        ...stateWithAudio.audio,
+        'talk_one_audio'
+      ])
+    })
+
+    it('if there is no audio in state, its created containing node audio', () => {
+      updated = handler(graph, state)
+      expect(updated.audio).toEqual([
+        'talk_one_audio'
+      ])
+    })
+
+  })
+
+  describe('if there is no audio in the node', () => {
+
+    const noAudioGraph = JSON.parse(JSON.stringify(graph))
+
+    beforeAll(() => {
+      delete noAudioGraph.processes.talk_one.metadata.audio
+    })
+
+    it('if there is audio in state, it remains the same', () => {
+      updated = handler(noAudioGraph, stateWithAudio)
+      expect(updated.audio).toEqual(stateWithAudio.audio)
+    })
+
+    it('if there is no audio in state, its created empty', () => {
+      updated = handler(noAudioGraph, state)
+      expect(updated.audio).toEqual([])
+    })
+
+  })
+
+})
